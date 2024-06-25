a3a5f7e8fc7011f847fd6a07f7c3ceaa1e1b54fa
Vednikov <aleasterxs@mail.ru>
2024-06-26 00:37:55 +0300

    fix linter causes

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -34,8 +34,7 @@ TEST_CASES = [
     (YAML_FILES['file_1'], YAML_FILES['file_2'], RESULTS['result_1']),
     (YAML_FILES['file_1'], YAML_FILES['file_3'], RESULTS['result_2']),
     (YAML_FILES['file_1'], YAML_FILES['file_4'], RESULTS['result_3']),
-    (YAML_FILES['file_5'], YAML_FILES['file_6'], RESULTS['result_4']),
-    ]
+    (YAML_FILES['file_5'], YAML_FILES['file_6'], RESULTS['result_4']),]
 
 
 @pytest.mark.parametrize('file_1_path, file_2_path, result_path', TEST_CASES)
@@ -44,5 +43,5 @@ def test_diff(file_1_path: str, file_2_path: str, result_path: str):
         correct_result = file.read()
 
     result = gd.generate_diff(file_1_path, file_2_path)
-    
+
     assert result == correct_result
